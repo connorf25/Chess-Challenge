6c4baffe3446a3627bc3208b42af245510c90e81
Connor Forbes <connorforbes25@gmail.com>
2023-08-30 23:46:29 +1000

    major: Positional logic to bot upgrades it big time

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using ChessChallenge.API;
 
 public class MyBot : IChessBot
@@ -13,23 +14,23 @@ public class MyBot : IChessBot
     {
         Move bestMove = Move.NullMove;
         // White wants to maximise score, while black wants to minimise
-        float bestValue = (board.IsWhiteToMove) ? float.MinValue : float.MaxValue;
+        double bestValue = (board.IsWhiteToMove) ? double.MinValue : double.MaxValue;
 
         var legalMoves = board.GetLegalMoves(capturesOnly: false);
 
         foreach (var move in legalMoves)
         {
             board.MakeMove(move);
-            float moveValue;
+            double moveValue;
 
             // If it's the bot's turn to play as white, then maximize; otherwise, minimize
             if (board.IsWhiteToMove)
             {
-                moveValue = Minimax(board, MAX_DEPTH - 1, float.MinValue, float.MaxValue, true);
+                moveValue = Minimax(board, MAX_DEPTH - 1, double.MinValue, double.MaxValue, true);
             }
             else
             {
-                moveValue = Minimax(board, MAX_DEPTH - 1, float.MinValue, float.MaxValue, false);
+                moveValue = Minimax(board, MAX_DEPTH - 1, double.MinValue, double.MaxValue, false);
             }
 
             board.UndoMove(move);
@@ -48,7 +49,7 @@ public class MyBot : IChessBot
     /**
      * Minimax search tree function
      */
-    float Minimax(Board board, int depth, float alpha, float beta, bool isMaximizing)
+    double Minimax(Board board, int depth, double alpha, double beta, bool isMaximizing)
     {
         if (depth == 0)
         {
@@ -57,12 +58,12 @@ public class MyBot : IChessBot
 
         if (isMaximizing)
         {
-            float maxEval = float.MinValue;
+            double maxEval = double.MinValue;
             var legalMoves = board.GetLegalMoves();
             foreach (var move in legalMoves)
             {
                 board.MakeMove(move);
-                float eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
+                double eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
                 board.UndoMove(move);
                 maxEval = Math.Max(maxEval, eval);
                 alpha = Math.Max(alpha, eval);
@@ -75,12 +76,12 @@ public class MyBot : IChessBot
         }
         else
         {
-            float minEval = float.MaxValue;
+            double minEval = double.MaxValue;
             var legalMoves = board.GetLegalMoves();
             foreach (var move in legalMoves)
             {
                 board.MakeMove(move);
-                float eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
+                double eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
                 board.UndoMove(move);
                 if (eval < minEval)
                 {
@@ -96,19 +97,85 @@ public class MyBot : IChessBot
         }
     }
 
+    // Array to hold the board evals for certain positions as white
+    private static double[][] pieceEvalWhite = new double[][]
+    {
+        // Pawn
+        new double[]
+        {
+            0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,
+            5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,
+            1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0,
+            0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5,
+            0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0,
+            0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5,
+            0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5,
+            0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0
+        },
+        // Knight
+        new double[]
+        {
+            -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0,
+            -4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0,
+            -3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0,
+            -3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0,
+            -3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0,
+            -3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0,
+            -4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0,
+            -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0
+        },
+        // Bishop
+        new double[]
+        {
+            -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0,
+            -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0,
+            -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0,
+            -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0,
+            -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0,
+            -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0,
+            -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0,
+            -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0
+        },
+        // Rook
+        new double[]
+        {
+            0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,
+            0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5,
+            0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5,
+            0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5,
+            0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5,
+            0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5,
+            0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5,
+            0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0
+        },
+        // Queen
+        new double[]
+        {
+            -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0,
+            -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0,
+            -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0,
+            -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5,
+             0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5,
+            -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0,
+            -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0,
+            -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0
+        },
+        // TODO: Look into king
+    };
+
     /**
      * Determine who the board state favours
      * This function counts pieces to determine who is winning
      * A positive value favours white, while a negative value favours black
      */
-    private float EvaluateBoard(Board board)
+    private double EvaluateBoard(Board board)
     {
-        float value = 0;
+        double value = 0;
 
         // Check checkmate
         if (board.IsInCheckmate())
         {
-            return board.IsWhiteToMove ? float.MaxValue : float.MinValue;
+            return board.IsWhiteToMove ? double.MaxValue : double.MinValue;
         }
 
         // Reward checks
@@ -118,31 +185,74 @@ public class MyBot : IChessBot
         }
 
         // If no checkmate evaluate other pieces
-        for (int pieceTypeInt = 1; pieceTypeInt <= 6; pieceTypeInt++)
+        for (int pieceTypeInt = 1; pieceTypeInt < 6; pieceTypeInt++)
         {
             var pieceType = (PieceType)pieceTypeInt;
-            var whitePieces = board.GetPieceList(pieceType, true);
-            var blackPieces = board.GetPieceList(pieceType, false);
 
-            switch (pieceType)
+            // Difference in number of pieces of type between white & black
+            var differenceInPieceCount =
+                board.GetPieceList(pieceType, true).Count
+                - board.GetPieceList(pieceType, false).Count;
+
+            // Positional evaluation array
+            var evalArrayWhite = pieceEvalWhite[pieceTypeInt - 1];
+            var evalArrayBlack = (double[])evalArrayWhite.Clone();
+            Array.Reverse(evalArrayBlack);
+
+            // Calculate new score using position
+            var pieceEvalScore =
+                // White pieces
+                EvaluateBitboard(board.GetPieceBitboard(pieceType, true), evalArrayWhite)
+                // Black pieces
+                - EvaluateBitboard(board.GetPieceBitboard(pieceType, false), evalArrayBlack);
+
+            // Multipy by number of pieces for each side
+            switch (pieceTypeInt)
             {
-                case PieceType.Pawn:
-                    value += (whitePieces.Count - blackPieces.Count) * 10;
+                case 1:
+                    pieceEvalScore += differenceInPieceCount * 10;
                     break;
-                case PieceType.Knight:
-                case PieceType.Bishop:
-                    value += (whitePieces.Count - blackPieces.Count) * 30;
+                case 2:
+                case 3:
+                    pieceEvalScore += differenceInPieceCount * 30;
                     break;
-                case PieceType.Rook:
-                    value += (whitePieces.Count - blackPieces.Count) * 50;
+                case 4:
+                    pieceEvalScore += differenceInPieceCount * 50;
                     break;
-                case PieceType.Queen:
-                    value += (whitePieces.Count - blackPieces.Count) * 90;
+                case 5:
+                    pieceEvalScore += differenceInPieceCount * 90;
                     break;
+                // No king for now
             }
+
+            // Add to value
+            value += pieceEvalScore;
 
         }
         return value;
+    }
+
+    /**
+     * Function to calculate the strength of the position
+     * of pieces of a certain type
+     */
+    private static double EvaluateBitboard(ulong bitboard, double[] evalArray)
+    {
+        double score = 0;
+
+        while (bitboard != 0)
+        {
+            // Get the index of the least significant bit that's set to 1
+            int index = BitOperations.TrailingZeroCount(bitboard);
+
+            // Add the corresponding value from the evalArray
+            score += evalArray[index];
+
+            // Clear the least significant bit that's set to 1
+            bitboard &= bitboard - 1;
+        }
+
+        return score;
     }
 
 }
