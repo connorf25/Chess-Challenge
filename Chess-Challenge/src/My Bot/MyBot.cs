1c7084f78ec31bddcabce7f693cb494d4b752997
Connor Forbes <connorforbes25@gmail.com>
2023-08-30 22:14:24 +1000

    fix: Use float instead of int

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,29 +3,33 @@ using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    private const int MAX_DEPTH = 3;
+    // How deep to search with minmax
+    private const int MAX_DEPTH = 4;
 
+    /**
+     * Main function to calculate next best move
+     */
     public Move Think(Board board, Timer timer)
     {
         Move bestMove = Move.NullMove;
         // White wants to maximise score, while black wants to minimise
-        int bestValue = (board.IsWhiteToMove) ? int.MinValue : int.MaxValue;
+        float bestValue = (board.IsWhiteToMove) ? float.MinValue : float.MaxValue;
 
         var legalMoves = board.GetLegalMoves(capturesOnly: false);
 
         foreach (var move in legalMoves)
         {
             board.MakeMove(move);
-            int moveValue;
+            float moveValue;
 
             // If it's the bot's turn to play as white, then maximize; otherwise, minimize
             if (board.IsWhiteToMove)
             {
-                moveValue = Minimax(board, MAX_DEPTH - 1, int.MinValue, int.MaxValue, true);
+                moveValue = Minimax(board, MAX_DEPTH - 1, float.MinValue, float.MaxValue, true);
             }
             else
             {
-                moveValue = Minimax(board, MAX_DEPTH - 1, int.MinValue, int.MaxValue, false);
+                moveValue = Minimax(board, MAX_DEPTH - 1, float.MinValue, float.MaxValue, false);
             }
 
             board.UndoMove(move);
@@ -41,7 +45,10 @@ public class MyBot : IChessBot
         return bestMove;
     }
 
-    int Minimax(Board board, int depth, int alpha, int beta, bool isMaximizing)
+    /**
+     * Minimax search tree function
+     */
+    float Minimax(Board board, int depth, float alpha, float beta, bool isMaximizing)
     {
         if (depth == 0)
         {
@@ -50,12 +57,12 @@ public class MyBot : IChessBot
 
         if (isMaximizing)
         {
-            int maxEval = int.MinValue;
+            float maxEval = float.MinValue;
             var legalMoves = board.GetLegalMoves();
             foreach (var move in legalMoves)
             {
                 board.MakeMove(move);
-                int eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
+                float eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
                 board.UndoMove(move);
                 maxEval = Math.Max(maxEval, eval);
                 alpha = Math.Max(alpha, eval);
@@ -68,12 +75,12 @@ public class MyBot : IChessBot
         }
         else
         {
-            int minEval = int.MaxValue;
+            float minEval = float.MaxValue;
             var legalMoves = board.GetLegalMoves();
             foreach (var move in legalMoves)
             {
                 board.MakeMove(move);
-                int eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
+                float eval = Minimax(board, depth - 1, alpha, beta, !isMaximizing);
                 board.UndoMove(move);
                 if (eval < minEval)
                 {
@@ -94,9 +101,9 @@ public class MyBot : IChessBot
      * This function counts pieces to determine who is winning
      * A positive value favours white, while a negative value favours black
      */
-    private int EvaluateBoard(Board board)
+    private float EvaluateBoard(Board board)
     {
-        int value = 0;
+        float value = 0;
 
         // Check checkmate
         if (board.IsInCheckmate())
@@ -120,22 +127,22 @@ public class MyBot : IChessBot
             switch (pieceType)
             {
                 case PieceType.Pawn:
-                    value += (whitePieces.Count - blackPieces.Count) * 1;
+                    value += (whitePieces.Count - blackPieces.Count) * 10;
                     break;
                 case PieceType.Knight:
                 case PieceType.Bishop:
-                    value += (whitePieces.Count - blackPieces.Count) * 3;
+                    value += (whitePieces.Count - blackPieces.Count) * 30;
                     break;
                 case PieceType.Rook:
-                    value += (whitePieces.Count - blackPieces.Count) * 5;
+                    value += (whitePieces.Count - blackPieces.Count) * 50;
                     break;
                 case PieceType.Queen:
-                    value += (whitePieces.Count - blackPieces.Count) * 9;
+                    value += (whitePieces.Count - blackPieces.Count) * 90;
                     break;
-                    // No king yet
             }
 
         }
         return value;
     }
+
 }
